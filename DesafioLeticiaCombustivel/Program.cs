5bf893fbfac15995db013ad990a3d0a8a772f161
hilton.lssantos <hilton.lssantos@RIP0576973W10-1>
2024-10-04 17:03:07 -0300

    Adicionar arquivos de projeto.

---
+++ DesafioLeticiaCombustivel/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesafioLeticiaCombustivel
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            //melhor opção de combustivel
+            double Op, vAlcool, vGasolina;
+
+            Console.WriteLine("Informe o valor do Alcool: ");
+            vAlcool = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Informe o valor da Gasolina: ");
+            vGasolina = Convert.ToDouble(Console.ReadLine());
+
+            Op = vAlcool / vGasolina;
+            //se o op<=0.7 melhor opção é o Alcool caso contrario é a Gasolina
+            if (Op <= 0.7)
+            {
+                Console.WriteLine("Melhor opção é Alcool!");
+            }
+            else
+            {
+                Console.WriteLine("Melhor opção é Gasolina!");
+            }
+            Console.ReadKey();
+        }
+    }
+}
